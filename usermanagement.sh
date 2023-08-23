d3cc20dc5ae89d6c855267423ae78f11d54f4efc
moaazEl <141212828+moaazEl@users.noreply.github.com>
2023-08-23 16:39:31 +1000

    Update usermanagement.sh

--- usermanagement.sh
+++ usermanagement.sh
@@ -1,72 +1,173 @@
-#!/bin/bash
-
- touch "existingusers.txt"
-sudo chmod 777 "existingusers.txt"
-touch "sortedexisting.txt"
-sudo chmod 777 sortedexisting.txt
-touch "sortedusers.txt"
-sudo chmod 777 sortedusers.txt
-touch "differences.txt"
-sudo chmod 777 differences.txt
-touch "removethese.txt"
-sudo chmod 777 removethese.txt
-touch "addthese.txt"
-sudo chmod 777 addthese.txt
-
-cut -d ':' -f 1 /etc/passwd > existingusers.txt
-sort existingusers.txt > sortedexisting.txt
-sort users.txt > sortedusers.txt
-
-diff "sortedusers.txt" "sortedexisting.txt" > "differences.txt"
-
-awk '/^< / {print $2}' differences.txt > removethese.txt
-awk '/^> / {print $2}' differences.txt > addthese.txt
+#!/bin/bash 
 
 
-# Path to the text file containing usernames
-input_file="addthese.txt"
+ 
+# Function to add users from authorizedusers.txt 
 
-# Check if the user list file exists
-if [ ! -f "$input_file" ]; then
-    echo "User list file $input_file not found."
-    exit 1
-fi
+add_users() { 
 
-# Read usernames from the input file and add users
-while IFS= read -r username; do
-    # Create the user with the given username
-    useradd -m "$username"
+    echo "Adding Users:" 
+
+    while IFS= read -r user; do 
+
+        sudo useradd "$user" 
+
+        echo "  $user" 
+
+    done < authorizedusers.txt 
+
+    echo 
+
+} 
+
   
-    if [ $? -eq 0 ]; then
-        echo "User $username added successfully."
-    else
-        echo "Failed to add user $username."
-    fi
-done < "$input_file"
 
-echo "User addition process completed."
+# Function to export current system users to systemusers.txt 
 
+export_system_users() { 
 
+    sudo awk -F':' '{print $1}' /etc/passwd > systemusers.txt 
 
-# Path to the text file containing usernames
-input_file2="deletethese.txt"
+} 
 
-# Check if the user list file exists
-if [ ! -f "$input_file" ]; then
-    echo "User list file $input_file not found."
-    exit 1
-fi
-
-# Read usernames from the input file and add users
-while IFS= read -r username; do
-    # delete the user with the given username
-    userdell -m "$username"
   
-    if [ $? -eq 0 ]; then
-        echo "User $username added successfully."
-    else
-        echo "Failed to add user $username."
-    fi
-done < "$input_file"
 
-echo "User addition process completed."
+# Function to sort users in systemusers.txt and save to systemusers2.txt 
+
+sort_system_users() { 
+
+    sort systemusers.txt > systemusers2.txt 
+
+} 
+
+  
+
+# Function to compare authorized users and system users 
+
+compare_users() { 
+
+    sudo diff authorizedusers.txt systemusers2.txt | grep '^<' | sed 's/^< //' 
+
+} 
+
+  
+
+# Function to add missing users from authorized users 
+
+add_missing_users() { 
+
+    echo "Users to Add:" 
+
+    while IFS= read -r user; do 
+
+        sudo useradd "$user" 
+
+        echo "  $user" 
+
+    done < <(compare_users) 
+
+    echo 
+
+} 
+
+  
+
+# Function to remove excess users from systemusers2.txt 
+
+remove_excess_users() { 
+
+    echo "Users to Delete:" 
+
+    while IFS= read -r user; do 
+
+        sudo userdel "$user" --remove 
+
+        echo "  $user" 
+
+    done < <(sudo diff systemusers2.txt authorizedusers.txt | grep '^>' | sed 's/^> //') 
+
+    echo 
+
+} 
+
+  
+
+# Function to perform cleanup 
+
+cleanup() { 
+
+    sudo rm -f systemusers.txt systemusers2.txt 
+
+    echo "Cleanup completed." 
+
+} 
+
+  
+
+# Confirmation prompt function 
+
+confirm() { 
+
+    read -p "Do you want to proceed? (yes/no): " choice 
+
+    case "$choice" in 
+
+        [Yy][Ee][Ss]|[Yy]) 
+
+            return 0 
+
+            ;; 
+
+        [Nn][Oo]|[Nn]) 
+
+            return 1 
+
+            ;; 
+
+        *) 
+
+            echo "Invalid choice. Please enter 'yes' or 'no'." 
+
+            confirm 
+
+            ;; 
+
+    esac 
+
+} 
+
+  
+
+# Check if authorizedusers.txt exists 
+
+if [ ! -f "authorizedusers.txt" ]; then 
+
+    echo "Create a sorted authorizedusers.txt file in order to execute the script." 
+
+    exit 1 
+
+fi 
+
+  
+
+# Main script execution 
+
+add_users 
+
+export_system_users 
+
+sort_system_users 
+
+add_missing_users 
+
+remove_excess_users 
+
+  
+
+# Display confirmation prompt 
+
+confirm && cleanup 
+
+  
+
+echo "User management completed." 
